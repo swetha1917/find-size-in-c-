25c55cf8f5f2bd8cda250ddff2a098f2fbecb671
swetha1917 <76477056+swetha1917@users.noreply.github.com>
2021-01-12 21:47:06 +0530

    developed to find the size using c++
    displays size of the required variable

---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ int main()
+{
+char a;
+int b;
+float c;
+double d;
+cout <<"size of char:"<<sizeof(a)<<" bytes"<<endl;
+
+cout <<"size of int:"<<sizeof(b)<<" bytes"<<endl;
+
+cout <<"size of float:"<<sizeof(c)<<" bytes"<<endl;
+
+cout <<"size of double:"<<sizeof(d)<<" bytes"<<endl;
+
+return 0;
+}
